20c1e4accd826dd79aa7b8f18c43761576e9c4bd
darylldan <dccaponpon@up.edu.ph>
2023-12-26 13:52:15 +0800

    feat: implemented a function table and integrated it into program object

---
+++ parser/function_table.py
@@ -0,0 +1,22 @@
+from lexer.token_class import TokenClass
+from parser.functions import FunctionStatement
+
+class FunctionTable():
+    def __init__(self):
+        self.func_table: dict[str, FunctionStatement] = {}
+
+    def is_func_defined(self, func_ident: str) -> bool:
+        return func_ident in self.func_table.keys()
+    
+    def add_func(self, func_ident: str, func_statement: FunctionStatement) -> bool:
+        if self.is_func_defined(func_ident):
+            return False
+        
+        self.func_table[func_ident] = func_statement
+        return True
+
+    def retrieve_func(self, func_ident: str) -> (FunctionStatement | None):
+        if not self.is_func_defined(func_ident):
+            return None
+        
+        return self.func_table[func_ident]
--- parser/program.py
+++ parser/program.py
@@ -1,6 +1,8 @@
 from lexer.token_class import TokenClass
 from parser.statement import Statement
 from parser.variable_list import VariableList
+from parser.function_table import FunctionTable
+from parser.functions import FunctionStatement
 
 class Program():
     def __init__(self):
@@ -8,6 +10,10 @@ class Program():
         self.variableList: VariableList = None
         self.statementList: list[Statement] = []
         self.kthxbye: TokenClass = None
+        self.func_table: FunctionTable = FunctionTable()
+
+    def add_func(self, func: FunctionStatement) -> bool:
+        return self.func_table.add_func(func.funcident.lexeme, func)
 
     def add_statement(self, statement: Statement):
         self.statementList.append(statement)
