4f7d2ce478632010ecf0a0aa7d50b6a4f6ecbd73
darylldan <dccaponpon@up.edu.ph>
2023-12-30 17:09:02 +0800

    fix: fixed type of value

--- semantics/symbol.py
+++ semantics/symbol.py
@@ -1,6 +1,7 @@
+from typing import Any
 from lexer.token_type import TokenType
 
 class Symbol():
-    def __init__(self, value: any, type: TokenType):
+    def __init__(self, value: Any, type: TokenType):
         self.value = value
         self.type = type
